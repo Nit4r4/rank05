a6b050a6bf631bccd909513ad42510db235e3a92
Nitara <87270463+Nit4r4@users.noreply.github.com>
2023-09-15 15:38:26 +0200

    Update Warlock.cpp

--- cpp_module_02/Warlock.cpp
+++ cpp_module_02/Warlock.cpp
@@ -21,7 +21,7 @@ void	Warlock::setTitle(std::string const &title) {
 }
 
 void	Warlock::introduce() const {
-	std::cout << _name << ": I am " << _name << ',' << _title << '!' << std::endl;
+	std::cout << _name << ": I am " << _name << ', ' << _title << '!' << std::endl;
 }
 
 void	Warlock::learnSpell(ASpell *spell) {
@@ -40,5 +40,5 @@ void	Warlock::launchSpell(std::string const &spellName, ATarget &target) {
 	if(spell) {
 		spell->launch(target);
 	}
-	// (_spellBook.createSpell(spellName))->launch(target);
+	// (_spellBook.createSpell(spellName))->launch(target); //works too
 }
